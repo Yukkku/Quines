0242b521e97c980d3749f2b2b9563b54a89dbcf7
Yukkku <76182771+Yukkku@users.noreply.github.com>
2022-02-07 20:44:49 +0900

    Create sugawara.js

---
+++ sugawara.js
@@ -0,0 +1,99 @@
+                                                  a =(b) =>{b= "        @@"; console.log("\x20"./*@@*/ repeat(
+                                               48+2)            +                     /*                      @@@
+                                            @@@@   */  "a\x20="+a+";a()")/*#a(h"^+Z|L({O";ZAGM.l7~jRUCr{^       dC6
+                                        +Cq.e   -:oXs  .e                                               u   q,S   u:x
+                                     WQc4   N$03(5 yz  Jk              vz                               f!   %6)D    O0
+                                  m?p.   E}[|z     @   mf           0U'Mb!K           @k  M   y ,S-K >7 W   PM  :31    vC
+                              U#ZX    Y|IVC        j{  4@              uh pX                       K  0 8@  {*    D]'    &^
+                             LHR   YvRZ            Y   Sm        |}g/O   ,Qa          l{>x1[8a 0SM7P2S|Q6   Y/       WZ0   *J
+                             pY/ ~?X               >a  0^      I['!0+@  r   pDr%           d            p9   M         @H    l9>
+                             mBG -f                R]  eQ           /- a         N8              gE!k   k}  kL           F)    F4H
+                             pta pO      &C%}w} <; W-   (zE)GkVMqk0   @1           yx              )_gDZF7   6             9g    utt
+                             S6X )/                J>   W               M            -     x_1,X    ;Ae "A  }^               S u  Zk
+                              fP L"                UZ  LQ               W             ) &+Q kAFl9.+GS$ l,4  gh     o*q       vC    -
+                              }R qw                l    2              *uP R6<         Ppzz  ht Kv @    zo  ]EV              W8q #~#
+                             l@g |h                g<   n           AHL          vm    =@               7gf  F               rIf   {
+                              Dj (W               .L{  D/         Eb                  j }   =X          'fT  Z               ,K7  3&
+                              0* 8h       ^FJw'o   rM  rG   j]f   h            ,rM.    (  )/j @2%(FcP2_ Z-   s               81   Zg
+                             CT& Q$      , 3_,< y& 6P   !  n(qNsV8R* H",?27Q  I &{9G49S%   !    O=@ @   ALu 5*K              nJ]   Z
+                              1D }b          [e@Yq xV   iQ  ~ s,ld                +I{dt35h4 pzp         fL   d               !K   z[
+                              HE MD             Q  =a  ^g  h ;  }B]_CA     FZTyH     :t' CjC  N         6-  IF               YR  ?)d
+                             b'q @u                T#  gC#    ! d x           ,      T P$ &    |]       d%  EU               !N   +3
+                              +~ X"                qO   5          AWJ6    iP#          w v5   I$ D     1f  {x               cT    |
+                             zrM c:       W   .    %K   s3ae d   t4D  r                  xk5?  H46gr9   Vr0 B: Sj>kF[Y       Yl    x
+                             (/[ n:          @     r*   .   )g   e .  /                 t K; ]|@nW!5H$7'&P  yYmXysdw)  S     H7   mK
+                              u8  b                _.   Vs          o   L>>~,r        TF^/$3   'j        ^f *;X               l   ;O
+                              ?  $*                lt  9$             d= 6 F 0        kWtg      e0       2; GQE              To   *F
+                              C  R'                E    Mk   g-oKSL   nk v&]n    L     ?Fr      q_F[0    U! 0;8              hIG   B
+                              aB o7                qe   K  k     -St   e               n?k    (J  $  'J03q[ X"y               1   0l
+                             KAF iK          Y>>2Je}   &{g  '    C 6 Z IaZh[1'k*   =  [!l   v1  sg!      )YlG2) il 91        ",Y  iN
+                             =yg ]S       }(r8>2K  ic  z&    $    a M  .(N<^{[^<! 7H)5    9g  fCk            }}; T]   .=#ZaM{ f    $
+                             I&0 OQ         e Crt+ o]  3$     A    cM dk'wg|qgvuS      p'G  ~S          L      !f             I#  cX
+                             h<  OZ                u}  E{     L    x 5   +m7x_t_   ht01   $i            =P       pY NCYU ~n*I I4  Jh
+                             KS  E~                #c  Xb    V {    25     f"PgA        <y              R         KS          E5  =B
+                             YEZ  X                *R  ]9 8(   d    * %    bTkv       [W               N           /  5 S X   b&   Q
+                             ^3  Om                s     /      !    E   I / $       *                E             (         A!  gk
+                             xi   y              _ /1  >         u   z 6z          Z6                I5             P         eT  Hu
+                             Tt  5>                CN v        @ $    B O         {         xIlU#)e^dg              h3       f62  Am
+                             2M  Q7       CgQf<!4  &!h        0   w    >*        /    0a:Cwd                         v        IZ  *0
+                             )=  vp      SZkQ- 01  X          ,    d   t F        *hl                                0        W}   =
+                             Ns  3g       T g,+62f<          =     $    H   o 0%p                                    V   #v   'r  XF|
+                             Lu  )r             3            ( <i@^#L3"(  Z  =xZF                                    doz5L:   +    O{
+                             iP  UH P ^Zmc$[X  1          'sJU2     &,  {r    %  1Y&_                                0#N^8uz6 dj  VX@
+                             f,m Rk          ?_       :L_k       zQD   g      hf     Z'D                             =d]uI    OD  W})
+                             jr  2m K<     }y      (Rv       {W7      j        h.      fkb                          r~? hK Z  n   oL8
+                             =X  ;:   [   po    q6$$       5[        o           I        [6                        @         Xb  $?X
+                             ix  0l     SP   2=   Cc    wF          ;,            %m        g@                      N         o0  =<8
+                             WU  Br    l^  )T  210   '/Z           CW              bl         qE                    1         Sv  |ZZ
+                             j#  dG   Y  V   _E(  'ND              p                 #          3C                 _h         kn  (Db
+                             {~  S4  !    b$i   <"                Y                   ~          Tw                 .         z   "Os
+                             8&  ~C *   ls    6p                ov                     8           Q'              #          gP  Mdj
+                             a,' ehA %2*    }X                    "e                    m           .,             b          N % x_t
+                             @= Bv <Z>    TU                      {_N                    P            i            9dk2E   R  I & @-b          p&*
+                             VZ Lt?     7P                         ^~t                    D            hJ          <TRGFs,    c ! tN=       }h[  X
+                              b[                                  Uu  i                    '             2        (G1m4p|yEM- f?@ O   nQ=!  vV fx
+                            71                                   j+   3Z%                   g             @       u6 q        k<k 2H@/ & 8 @
+                          |?                                     ;6    2 gU                 B              W      3        u EIc   8K:K
+                        r5                                      p <     vj }cY               {              V     x t/8r91~ge0) x y-T
+                      *(        9yr$_.irA.|fCBvF}qS999)l(4      - .       '   ch                             %   X I  iWwZ'E  u+t CuC
+                     a4   rPj%"            D             lm&tS L  S         ?  w g/                              x+-2         1s: 0ZT
+                    SDKl@E         ;$]8T$  O            NIR   cw  &         xe K    "Drk       j                v  &          yE&  'x
+                    Y  ^A    UO;J        bmA          E>#     I   (          o ?z       z8H7                    YB  1"-       ^%0 Fl~
+                             'dR@          I}       2uz      KBU){p{5PtXRIqCThp {        8   ^K                !pMX    >u     >lo UF9
+                              {[)         @nl    1(>       m'%nAX5x;q -cg  DY ^  ,        x    hf              f  _       z   a-9 6Rw
+                           ErB- !      Nbt5    xO       O{@E S ]b kh* A@)Dif94aX  ~        S     <T            .  Z        B  $jt bRm
+                       L)Qa     7  f:P+      ;|      Es%7uqUV0O Z *F1O 3C+/G>x99   6       N{   t D           W)   ,        l .O   r,
+                     e%I       ($"u{ KJ    Ls      GO  $vS  ^f]c k{  cZR <S'ZU;9   F        b~JP              {    T         K.#" 1bxN('X
+                    0R          q    Z   N}       L  IB    !my af .  p 3 Y d"JyXq  KM      <'                Z     M          }     *TYaU
+                   .           1     *  ?       s]        T/A D )Ti&=&#~ Eg hL1gxe  M      @                 *    WZ           i ?5<DXPg|
+                  E             i!   x G       Al  =       hBG HS9 U. $  v       B FJi     1                j     Z            !($q|#Q OT
+                 *                   MD  ;eSeA,        Au  0        { C2    RzfI  B)  4    %v              [t     Y             O[R,t1(vvR
+                 5                       -   Ez '?&ckb>B m    z*  3 6 g3        9xJ    4H   ]!             Z      f             h 6<Zwdw
+                P                      Ms      #       h >  !     /Q T[H         j      +e    _           -      [k             *{|TA
+                Y                   7jz       R        6  ]  b       304          l      =(    D2         "      D              [.
+               b                 nq;         Eb        )  5  D   Iq  y!'           b       FA1  0"       ,       ^               x
+              gN               x%            i         }  !   d     @04?            %V            |      B       %               g
+           8d=C              K&             {        ;z0 t _  #       6E              64           ]/   S aD7[y =D               H
+       jw>;   z           2Kr             GR       ;H    f z  XN  E2  1                 )]          AY 0t ln   s@'+T             "
+    O#z      Z         >=h                E      u$   .X9  y   P   5   U                  <xzw       f-x   7        qb?>         hR
+ #Ini+a(&,9OP6      qV4                  2     "u   C$  R  6P  Z KA%  k%(                     e14*    I     5           hMF      ?1
+ Xi1VINhw3frV   _Si-                     4    o  ?mK(X)f58A "  S]   t  4t                        #B'{R       5           8nB&"  sYx
+ C4HAM*CP. f                             :   ~  H  E2  T fHsD  jo   ,nPF T                                    *           q=Q3@eLi
+ %l-^iJN| ][                              D#9S i-  H6O ,K ,N6Sj ' &N      y                                                j=)
+  0lLX'L0]3 W :                               z 9(F : KUb47^x+{NWka01|H(KP',                           &^?wajKjoOV          ?
+       k    0  9L!                           jA X +kc>T~@( ] %             f                       C6wD^      _HG            c
+        e   a hnZ 12                        #(4  9AfsXc0XzL8,R              5G3Dn             %O[j;d~ (    TBQ           uKTwUUl
+        :~ m     9{ d9                     lO6  Xi$K YPY 9 E"f                    QK#+0dC6J  NI      HZ7^-         one#3q=     mRgb
+        }ZVX      IU  -F                 ]zQ   X/ekC"|m= o, -:                    K     =! FYe,        8     a6fb(X              (X
+       lhV6     2       e;?            OxH     9%)  K%v v~F&Ja                    :cD17><!gXRtqY        Cr{QK     L            4~
+       ,r(      l<       e<&1        z_       a/Y:WAZ|KtkJ{',                    $M -xA kSBZp aF                  ;        5q,
+        zO           F4    <ish[   [)        H^mgB#ya  J2~ M6                    Hh]+RWOnrg"4*f-             tSD   N    Huf
+       f >Q .6F|S              ?_%O           F  x4                              ihl<*>CVx>e<                  Oh)b|  V^
+         i        ea!1/^Sb2   G  h                                                                                J4V7
+       Lm&[5B!0G=7/8VVqL [So%;
+        2               {  <
+      r;                   k
+    Z0                $HQ~M
+ w2O             eP)04
+"'           SXOm
+  >Le2*/};a()
